574e6650a00b66b598189c4c1ea3defbed8d1541
RasmiJati <jatirasmi65@gmail.com>
2023-03-29 03:51:45 +0100

    declare array of size 5 and display the value of this array while initializing and taking input from user

--- src/basicjavaprogram/DeclareAndDisplayArrayWhileInitializing.java
+++ src/basicjavaprogram/DeclareAndDisplayArrayWhileInitializing.java
@@ -5,20 +5,30 @@
  */
 package basicjavaprogram;
 
+import java.util.Scanner;
+
 /**
  *
  * @author admin
  */
 public class DeclareAndDisplayArrayWhileInitializing {
+
     public static void main(String[] args) {
         int arr[] = new int[5]; //let size of array is 5
         //adding value to array
-        arr[0] = 5;
-        arr[1] = 10;
-        arr[2] = 9;
-        arr[3] = 65;
-        arr[4] = 2;
-        
+//        now taking input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide value for lst element for array : ");
+        arr[0] = sc.nextInt();
+        System.out.println("Provide value for 2nd element for array : ");
+        arr[1] = sc.nextInt();
+        System.out.println("Provide value for 3rd element for array : ");
+        arr[2] = sc.nextInt();
+        System.out.println("Provide value for 4th element for array : ");
+        arr[3] = sc.nextInt();
+        System.out.println("Provide value for 5th element for array : ");
+        arr[4] = sc.nextInt();
+
         System.out.println("second element  : " + arr[1]);
     }
 }
