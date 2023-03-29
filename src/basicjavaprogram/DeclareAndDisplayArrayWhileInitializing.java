fef3f310f5bc6c689fa0aaf8ee9186f43f9b9dd2
RasmiJati <jatirasmi65@gmail.com>
2023-03-29 04:13:11 +0100

    declare array of size 5 and display the value of this array while initializing and taking input from user and using foor loop

--- src/basicjavaprogram/DeclareAndDisplayArrayWhileInitializing.java
+++ src/basicjavaprogram/DeclareAndDisplayArrayWhileInitializing.java
@@ -15,20 +15,29 @@ public class DeclareAndDisplayArrayWhileInitializing {
 
     public static void main(String[] args) {
         int arr[] = new int[5]; //let size of array is 5
+//        using loop in array
+        Scanner sc = new Scanner(System.in);
+        int i;
+        for(i=0;i<5;i++){
+            System.out.println("Provide value for "+ i + "th element for array");
+            arr[i] = sc.nextInt();
+            System.out.println("Value of array : " + arr[i]);
+        }
+
         //adding value to array
 //        now taking input from user
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Provide value for lst element for array : ");
-        arr[0] = sc.nextInt();
-        System.out.println("Provide value for 2nd element for array : ");
-        arr[1] = sc.nextInt();
-        System.out.println("Provide value for 3rd element for array : ");
-        arr[2] = sc.nextInt();
-        System.out.println("Provide value for 4th element for array : ");
-        arr[3] = sc.nextInt();
-        System.out.println("Provide value for 5th element for array : ");
-        arr[4] = sc.nextInt();
-
-        System.out.println("second element  : " + arr[1]);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Provide value for lst element for array : ");
+//        arr[0] = sc.nextInt();
+//        System.out.println("Provide value for 2nd element for array : ");
+//        arr[1] = sc.nextInt();
+//        System.out.println("Provide value for 3rd element for array : ");
+//        arr[2] = sc.nextInt();
+//        System.out.println("Provide value for 4th element for array : ");
+//        arr[3] = sc.nextInt();
+//        System.out.println("Provide value for 5th element for array : ");
+//        arr[4] = sc.nextInt();
+//
+//        System.out.println("second element  : " + arr[1]);
     }
 }
