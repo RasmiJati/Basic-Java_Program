e96521f03763895a83ba78c4b96f145f4371dabd
RasmiJati <jatirasmi65@gmail.com>
2023-03-29 05:00:59 +0100

    display sum of even numbers and product of odd numbers from 15 to 200 method 2

--- src/basicjavaprogram/SumEvenAndProductOddFromRangeOfNumbers.java
+++ src/basicjavaprogram/SumEvenAndProductOddFromRangeOfNumbers.java
@@ -14,23 +14,15 @@ package basicjavaprogram;
 public class SumEvenAndProductOddFromRangeOfNumbers {
 
     public static void main(String[] args) {
-//        start = 15;
-//        end = 200 ;
         int sum = 0;
         int product = 1;
         for (int i = 15; i <= 200; i++) {
             if (i % 2 == 0) {
                 sum += i;
+            }else {
+                product *= i;
             }
         }
-        System.out.println("Sum of even numbers : " + sum);
-
-        for (int j = 15; j <= 200; j++) {
-            if (j % 2 != 0) {
-                product *= j;
-            }
-        }
-        System.out.println("Product of odd numbers : " + product);
-
+        System.out.println("Sum of even numbers : " + sum + "\n Product of odd numbers : " + product);
     }
 }
